ec35abf2cb581a6f5c63890d56ea548fc4226d3e
2200030217 <168654451+2200030217@users.noreply.github.com>
2024-10-10 18:42:55 +0530

    Add files via upload

---
+++ src/Components/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import "./intro.css"
+
+function Intro() {
+  return (
+    <div className='intro-container'>
+      <h1>Welcome to Handloom Fashion Website!</h1>
+    </div>
+  );
+}
+
+export default Intro;
